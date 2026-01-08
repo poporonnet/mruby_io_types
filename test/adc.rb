f53c24b25c008a5f6165bbe0b029a75601925273
tufusa <me@tufusa.net>
2026-01-08 15:25:28 +0900

    feat: ADC クラスを追加

---
+++ test/adc.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/UselessAssignment
+
+# ADC.new
+adc1 = ADC.new 1
+
+# ADC#read_voltage
+v1 = adc1.read_voltage
+
+# ADC#read
+v1 = adc1.read
+
+# ADC#read_raw
+v1 = adc1.read_raw
+
+# rubocop:enable all
